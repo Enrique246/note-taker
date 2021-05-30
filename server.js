3ba1e46a7f7b97d6157eaf10529f8c323d9f466a
Henri <ecch9210@gmail.com>
2021-05-30 09:52:18 -0500

    Parameters pushed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 // Sets up the Express App
 const application = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 const apiRout = require ("./routes/ApiRoutes");
 const htmlRout = require ("./routes/HtmlRoutes");
